65d71502bcd4268e09f9fb2b330906e16f7de7b9
Peter Gasparik <gasparik.pe@gmail.com>
2020-06-12 19:04:09 +0200

    decrease hex code to 8 bytes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,7 +10,9 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->app->singleton(HexadecimalGenerator::class, HexadecimalGenerator::class);
+        $this->app->singleton(HexadecimalGenerator::class, function () {
+            return new HexadecimalGenerator(8);
+        });
 
         $this->app->singleton(ListenerPatternMatcher::class, ListenerPatternMatcher::class);
     }
