9f7a03562c21b97b00201af3bcfc2c2fa6771487
Peter Gasparik <gasparik.pe@gmail.com>
2020-06-12 19:21:51 +0200

    encrypt custom rule data

--- app/Http/ResponseSchema/ListenerResponseSchemeAdapter.php
+++ app/Http/ResponseSchema/ListenerResponseSchemeAdapter.php
@@ -15,7 +15,7 @@ class ListenerResponseSchemaAdapter implements Adapter
             'rules' => $item->getRules(),
             'handler' => [
                 'slug' => $item->getHandlerSlug(),
-                'values' => $item->getHandlerValues()
+                'values' => decrypt($item->getHandlerValues())
             ]
         ];
     }
